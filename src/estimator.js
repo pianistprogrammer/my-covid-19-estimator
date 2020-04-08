fb97ffcf34b1ec3ddeb3cb648df254d0ebc0ac55
Abimbola Jeremiah <jeremiahabimbola0@gmail.com>
2020-04-08 18:43:43 +0800

    modified the estimator file

--- src/estimator.js
+++ src/estimator.js
@@ -1,68 +1,91 @@
-const covid19ImpactEstimator = (data) => {
-
-    let estimateCurrentlyInfected = (data) => {
-        return {
-            data: data.reportedCases,
-            impact: {
-                currentlyInfected: reportedCases * 10
-            },
-            severeImpact: {
-                currentlyInfected: reportedCases * 50
-            }
+const estimateCurrentlyInfected = (data) => {
+    return {
+        data: data,
+        impact: {
+            currentlyInfected: data.reportedCases * 10
+        },
+        severeImpact: {
+            currentlyInfected: data.reportedCases * 50
         }
     }
-    let estimateProjectedInfections = (data) => {
-        const currentlyInfectedEstimate = estimateCurrentlyInfected();
-        const periodType = data.periodType;
-        if (periodType == 'days') {
-            return {
-                data: data,
-                impact: {
-                    infectionsByRequestedTime: currentlyInfectedEstimate.impact.currentlyInfected * Math.floor(Math.pow(2, 0.3))
-                },
-                severeImpact: {
-                    infectionsByRequestedTime: currentlyInfectedEstimate.severeImpact.currentlyInfected * Math.floor(Math.pow(2, 0.3))
-                }
-            }
-        }
-        else if (periodType == 'weeks') {
-            return {
-                data: data,
-                impact: {
-                    infectionsByRequestedTime: currentlyInfectedEstimate.impact.currentlyInfected * Math.floor(Math.pow(2, 0.3) * 7)
-                },
-                severeImpact: {
-                    infectionsByRequestedTime: currentlyInfectedEstimate.severeImpact.currentlyInfected * Math.floor(Math.pow(2, 0.3) * 7)
-                }
-            }
-        }
-        else {
-            return {
-                data: data,
-                impact: {
-                    infectionsByRequestedTime: currentlyInfectedEstimate.impact.currentlyInfected * Math.floor(Math.pow(2, 0.3) * 30)
-                },
-                severeImpact: {
-                    infectionsByRequestedTime: currentlyInfectedEstimate.severeImpact.currentlyInfected * Math.floor(Math.pow(2, 0.3) * 30)
-                }
-            }
-        }
-    }
-    let estimateSevereCases = (data) => {
-
-        const projectedInfections = estimateProjectedInfections();
+};
+const estimateProjectedInfections = (data) => {
+    const currentlyInfectedEstimate = estimateCurrentlyInfected();
+    const { periodType } = data;
+    if (periodType === 'days') {
         return {
             data: data,
             impact: {
-                severeCasesByRequestedTime: projectedInfections.impact.infectionsByRequestedTime * 0.15,
-                hospitalBedsByRequestedTime: 0.35 * data.totalHospitalBeds - severeCasesByRequestedTime
+                infectionsByRequestedTime: currentlyInfectedEstimate.impact.currentlyInfected * Math.floor(Math.pow(2, 0.3))
             },
             severeImpact: {
-                severeCasesByRequestedTime: projectedInfections.severeImpact.infectionsByRequestedTime * 0.15,
-                hospitalBedsByRequestedTime: data.totalHospitalBeds * 512 - severeCasesByRequestedTime
+                infectionsByRequestedTime: currentlyInfectedEstimate.severeImpact.currentlyInfected * Math.floor(Math.pow(2, 0.3))
+            }
+        }
+    }
+    else if (periodType === 'weeks') {
+        return {
+            data: data,
+            impact: {
+                infectionsByRequestedTime: currentlyInfectedEstimate.impact.currentlyInfected * Math.floor(2 ** 0.3 * 7)
+            },
+            severeImpact: {
+                infectionsByRequestedTime: currentlyInfectedEstimate.severeImpact.currentlyInfected * Math.floor(Math.pow(2, 0.3) * 7)
+            }
+        }
+    }
+    else {
+        return {
+            data: data,
+            impact: {
+                infectionsByRequestedTime: currentlyInfectedEstimate.impact.currentlyInfected * Math.floor(Math.pow(2, 0.3) * 30)
+            },
+            severeImpact: {
+                infectionsByRequestedTime: currentlyInfectedEstimate.severeImpact.currentlyInfected * Math.floor(Math.pow(2, 0.3) * 30)
             }
         }
     }
 };
+const estimateSevereCases = (data) => {
+    const projectedInfections = estimateProjectedInfections();
+    return {
+        data: data,
+        impact: {
+            severeCasesByRequestedTime: projectedInfections.impact.infectionsByRequestedTime * 0.15
+        },
+        severeImpact: {
+            severeCasesByRequestedTime: projectedInfections.severeImpact.infectionsByRequestedTime * 0.15
 
+        }
+    }
+};
+const estimatedBedspaceAvailability = (data) => {
+    const availability = estimateSevereCases();
+    return {
+        data: data,
+        impact: {
+            hospitalBedsByRequestedTime: 0.35 * data.totalHospitalBeds - availability.impact.severeCasesByRequestedTime
+        },
+        severeImpact: {
+            hospitalBedsByRequestedTime: data.totalHospitalBeds * 512 - availability.impact.severeCasesByRequestedTime
+        }
+    }
+};
+const covid19ImpactEstimator = (data) => {
+    const estimator = chain(
+
+        //Challenge 1
+        estimateCurrentlyInfected,
+        estimateProjectedInfections,
+
+        // Challenge 2
+        estimateSevereCases,
+        estimatedBedspaceAvailability
+    )
+    return estimator({
+        data: {},
+        impact: {},
+        severeImpact: {}
+    });
+};
 export default covid19ImpactEstimator;
