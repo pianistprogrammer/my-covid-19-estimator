37305d1afc76cd730dbcfe69c28eebc0590e520a
Abimbola Jeremiah <jeremiahabimbola0@gmail.com>
2020-04-08 18:19:11 +0800

    removed data

--- src/estimator.js
+++ src/estimator.js
@@ -1,17 +1,5 @@
 const covid19ImpactEstimator = (data) => {
-    const data = {
-        region: {
-            name: "Africa",
-            avgAge: 19.7,
-            avgDailyIncomeInUSD: 5,
-            avgDailyIncomePopulation: 0.71
-        },
-        periodType: "days",
-        timeToElapse: 58,
-        reportedCases: 674,
-        population: 66622705,
-        totalHospitalBeds: 1380614
-    }
+
     let estimateCurrentlyInfected = (data) => {
         return {
             data: data.reportedCases,
