03ca4c7cf0d2c6d9b6b67d1ab5493d21f7884db6
Abimbola Jeremiah <jeremiahabimbola0@gmail.com>
2020-04-13 14:42:34 +0800

    changes to app

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-// const helmet = require('helmet');
 
-
-// const filePath = path.join(__dirname, 'audit.log');
-// const accessLogStream = fs.createWriteStream(filePath, { flags: 'a' });
+const filePath = path.join(__dirname, 'audit.log');
 
 const app = express();
 
@@ -14,10 +11,9 @@ const bodyParser = require('body-parser');
 
 const estimatorRoutes = require('./src/backend/estimator');
 
-
 app.use(
-  morgan(':method :url    :status  :response-time ms', {
-    stream: fs.createWriteStream(path.join(__dirname, 'audit.log'), {
+  morgan(':method :url :status :response-time ms', {
+    stream: fs.createWriteStream(filePath, {
       flags: 'a'
     })
   })
@@ -26,30 +22,6 @@ app.use(morgan('tiny'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-// app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
-
-// app.use(helmet());
-// const logger = morgan(
-//   (tokens, req, res) => {
-//     let responseTime = Math.ceil(tokens['response-time'](req, res))
-//       .toString()
-//       .padStart(2, 0);
-//     responseTime = `${responseTime}ms`;
-//     return [
-//       tokens.method(req, res),
-//       tokens.url(req, res),
-//       tokens.status(req, res),
-//       responseTime
-//     ].join(' ');
-//   },
-//   {
-//     stream: accessLogStream,
-//     skip: (_, res) => res.statusCode === 404
-//   }
-// );
-// app.use(logger);
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
 
 // Routes to handle requests
 app.use('/api/v1/on-covid-19', estimatorRoutes);
