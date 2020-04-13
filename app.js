d6d7cde3dec2276f0bd96141622261f36bf3e83e
Abimbola Jeremiah <jeremiahabimbola0@gmail.com>
2020-04-13 14:24:26 +0800

    chnges

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const helmet = require('helmet');
+
 
 const filePath = path.join(__dirname, 'audit.log');
 const accessLogStream = fs.createWriteStream(filePath, { flags: 'a' });
-
 
 const app = express();
 
@@ -13,6 +14,7 @@ const bodyParser = require('body-parser');
 
 const estimatorRoutes = require('./src/backend/estimator');
 
+app.use(helmet());
 const logger = morgan(
   (tokens, req, res) => {
     let responseTime = Math.ceil(tokens['response-time'](req, res))
@@ -32,8 +34,8 @@ const logger = morgan(
   }
 );
 app.use(logger);
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Routes to handle requests
 app.use('/api/v1/on-covid-19', estimatorRoutes);
