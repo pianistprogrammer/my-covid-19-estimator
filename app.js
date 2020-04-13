6bdb9e338c6743d4b12d86267c2f7fa2abd38745
Abimbola Jeremiah <jeremiahabimbola0@gmail.com>
2020-04-13 13:54:29 +0800

    changes to app log

--- app.js
+++ app.js
@@ -1,27 +1,40 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const helmet = require('helmet');
+
 
 const filePath = path.join(__dirname, 'audit.log');
 
 const app = express();
 
 const morgan = require('morgan');
-const bodyParser = require('body-parser');
-
 const estimatorRoutes = require('./src/backend/estimator');
 
-app.use(
-  morgan(':method :url :status :response-time ms', {
-    stream: fs.createWriteStream(filePath, {
-      flags: 'a'
-    })
-  })
-);
-app.use(morgan('tiny'));
+const accessLogStream = fs.createWriteStream(filePath, { flags: 'a' });
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(helmet());
+const logger = morgan(
+  (tokens, req, res) => {
+    let responseTime = Math.ceil(tokens['response-time'](req, res))
+      .toString()
+      .padStart(2, 0);
+    responseTime = `${responseTime}ms`;
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      responseTime
+    ].join(' ');
+  },
+  {
+    stream: accessLogStream,
+    skip: (_, res) => res.statusCode === 404
+  }
+);
+app.use(logger);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Routes to handle requests
 app.use('/api/v1/on-covid-19', estimatorRoutes);
